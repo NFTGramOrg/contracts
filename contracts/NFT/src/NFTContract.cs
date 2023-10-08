5a659b8c7f23cf1cd9ba5be73dd8eb6f7b6fdd34
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-10-08 11:19:00 +0530

    first commit

---
+++ contracts/NFT/src/NFTContract.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.ComponentModel;
+using System.Numerics;
+
+using Neo;
+using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Attributes;
+using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Services;
+
+namespace NFT
+{
+    [DisplayName("Gabriel.NFTContract")]
+    [ManifestExtra("Author", "Gabriel Antony Xaviour")]
+    [ManifestExtra("Email", "gabrielantony56@gmail.com")]
+    [ManifestExtra("Description", "This is a Pokemon NFT contract")]
+    [SupportedStandards("NEP-11")]
+    [ContractPermission("*", "onNEP11Payment")]
+
+    public class PokemonState : Nep11TokenState
+    {
+        public string Description;
+        public string Image;
+    }
+
+    public class NFTContract : Nep11Token<PokemonState>
+    {
+        const byte Prefix_NumberStorage = 0x00;
+        const byte Prefix_ContractOwner = 0xFF;
+
+        public override string Symbol() => "POKEMON";
+
+        private static Transaction Tx => (Transaction) Runtime.ScriptContainer;
+        
+        [DisplayName("_deploy")]
+        public static void Deploy(object data, bool update)
+        {
+            if (update) return;
+
+            var key = new byte[] { Prefix_ContractOwner };
+            Storage.Put(Storage.CurrentContext, key, Tx.Sender);
+        }
+        
+        public static void Update(ByteString nefFile, string manifest)
+        {
+            var key = new byte[] { Prefix_ContractOwner };
+            var contractOwner = (UInt160)Storage.Get(Storage.CurrentContext, key);
+
+            if (!contractOwner.Equals(Tx.Sender))
+            {
+                throw new Exception("Only the contract owner can update the contract");
+            }
+
+            ContractManagement.Update(nefFile, manifest, null);
+        }
+    }
+}
